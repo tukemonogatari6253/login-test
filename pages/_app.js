24d18ae0335f1bcced35f96f52b6ba006bd0473f
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-04-21 02:07:12 +0900

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+// pages/_app.js
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
+}
