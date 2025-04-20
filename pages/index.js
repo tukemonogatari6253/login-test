a9dd28052f7f5dda51099ba8fa3b204b63989010
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-04-21 01:08:09 +0900

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Link from 'next/link';
+
+export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  return (
+    <div>
+      <h1>Welcome</h1>
+      {!user ? (
+        <a href="/api/auth/login">Login</a>
+      ) : (
+        <>
+          <p>Hello, {user.name}</p>
+          <a href="/api/auth/logout">Logout</a>
+          <br />
+          <Link href="/protected">Go to Protected Page</Link>
+        </>
+      )}
+    </div>
+  );
+}
