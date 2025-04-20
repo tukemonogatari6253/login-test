4625ac5da673ddf092c0357af398682c5a79b82c
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-04-21 01:09:02 +0900

    Create protected.js

---
+++ pages/protected.js
@@ -0,0 +1,18 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { getMicroCMSData } from '../lib/microcms';
+
+export default function Protected({ data }) {
+  return (
+    <div>
+      <h1>Protected Page</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
+
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps() {
+    const data = await getMicroCMSData();
+    return { props: { data } };
+  }
+});
