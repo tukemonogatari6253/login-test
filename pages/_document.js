6ce9942c59c9127fa0ea164ff554810b648cbcb4
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-04-30 00:01:58 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="ja">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="ja"> {/* ← ここで言語を指定 */}
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
